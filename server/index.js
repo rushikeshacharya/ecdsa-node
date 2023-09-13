bddb86a2aa4f70c1ea3ff7b4d45fa674d7cfeba6
Rushikesh Acharya <v-racharya@microsoft.com>
2023-09-13 13:39:14 +0530

    feat: Week 1 Assignment

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3042;
+const {keys} = require("./keys.json");
+app.use(cors());
+app.use(express.json());
+
+// console.log('keys', keys[0]);
+const balances = {
+  [keys[0].address] : 100 ,
+  [keys[1].address]: 50,
+  [keys[2].address]: 20,
+};
+
+console.log(balances);
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
+  const balance = balances[address] || 0;
+  res.send({ balance });
+});
+
+app.post("/send", (req, res) => {
+  const { sender, recipient, amount } = req.body;
+
+  setInitialBalance(sender);
+  setInitialBalance(recipient);
+
+  if (balances[sender] < amount) {
+    res.status(400).send({ message: "Not enough funds!" });
+  } else {
+    balances[sender] -= amount;
+    balances[recipient] += amount;
+    res.send({ balance: balances[sender] });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
+function setInitialBalance(address) {
+  if (!balances[address]) {
+    balances[address] = 0;
+  }
+}
